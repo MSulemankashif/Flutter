f0206ac97b239dd68ca37d2c56752d252f151682
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2025-07-17 18:58:47 +0500

    Update home.dart

--- fetchapi/lib/pages/home.dart
+++ fetchapi/lib/pages/home.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  
+
   ApiService apiService = ApiService();
   TextEditingController titleController = TextEditingController();
   TextEditingController descController = TextEditingController();
   @override
-  Widget build
-  (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber.shade700,
-        title: Text("MyNotes"),
+        title: Text("My Notes"),
       ),
 
-      floatingActionButton: FloatingActionButton(onPressed: 
-      showFormDialoge(context)),
-    )
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showFormDialoge(context);
+      },
+      foregroundColor: Colors.black,
+      backgroundColor: Colors.amber.shade700,
+      child: Icon(Icons.add),
+      ),
+    );
   }
+
+  showFormDialoge(BuildContext context){
+
+    showDialog(
+    context: context, 
+    builder: (context){
+      return AlertDialog(
+        title: Text("Add Notes"),
+        content: Column(
+          mainAxisAlignment: MainAxisSize.min,
+          children: [
+
+            // title
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                hintText: "Enter Title",
+                border: OutlineInputBorder()
+              ),
+            ),
+
+          
+
+          ],
+        ),
+      )
+    });
+  }
+
+
+
+
 }
